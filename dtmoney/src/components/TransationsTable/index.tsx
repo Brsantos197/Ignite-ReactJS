073f41e9493bcdb88b05ebf9e3cc3d1ede65060b
Brsantos197 <brsantos197@gmail.com>
2021-06-18 01:59:55 -0300

    Contextos e hooks: Carregando transações

--- dtmoney/src/components/TransationsTable/index.tsx
+++ dtmoney/src/components/TransationsTable/index.tsx
@@ -1,58 +1,42 @@
-import { useContext, useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useContext } from "react";
 import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
-interface Transaction {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createdAt: string;
-}
-
 export function TransationsTable() {
-	const data = useContext(TransactionsContext);
-	const [transactions, setTransactions] = useState<Transaction[]>([]);
+	const transactions = useContext(TransactionsContext);
 
-	useEffect(() =>{
-			api.get('transactions')
-					.then(response => setTransactions(response.data.transactions))
-	}, [])
 	return(
-			<Container>
-					<table>
-							<thead>
-									<tr>
-											<th>Título</th>
-											<th>Valor</th>
-											<th>Categoria</th>
-											<th>Data</th>
-									</tr>
-							</thead>
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
 
-							<tbody>
-									{transactions.map(transaction => (
-											<tr key={transaction.id}>
-													<td>{transaction.title}</td>
-													<td className={transaction.type}>
-                            {new Intl.NumberFormat('pt-BR', {
-                              style: 'currency',
-                              currency: 'BRL'
-                            }).format(transaction.amount)}
+                <tbody>
+                    {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(transaction.amount)}
+                            </td> 
+                            <td>{transaction.category}</td>
+                            <td>
+                                {new Intl.DateTimeFormat('pt-BR').format(
+                                new Date(transaction.createdAt)
+                                )}
                             </td>
-													<td>{transaction.category}</td>
-													<td>
-                          {new Intl.DateTimeFormat('pt-BR').format(
-                            new Date(transaction.createdAt)
-                          )}
-                          </td>
-											</tr>
-									)
-									)}
-							</tbody>
-					</table>
-			</Container>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Container>
 	)
 }
