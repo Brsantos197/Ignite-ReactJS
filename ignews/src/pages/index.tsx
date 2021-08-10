4a0f94b17ce138d43e2f73b1086054e456008624
Brsantos197 <brsantos197@gmail.com>
2021-08-10 01:47:14 -0300

    Estrutura da aplicação: Estilos globais do app

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,16 +1,14 @@
 import Head from "next/head";
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (  
     <>
       <Head>
           <title>Início | ig.news</title>
         </Head>
-      <h1 className={styles.title}>
+      <h1>
         
-        Hello <span>World</span> 
+        Hello World
       </h1>
     </>
   )
