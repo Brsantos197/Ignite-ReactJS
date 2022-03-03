5362e674a01d2fad7c4d96ec6466e0b24e7b2bc0
Brsantos197 <brsantos197@gmail.com>
2022-03-03 02:01:32 -0300

    API Routes no Next.js | Autenticação com Next Auth

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/app'
 import { Header } from '../components/Header';
+import { SessionProvider as NextAuhtProvider } from "next-auth/react";
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuhtProvider session={pageProps.session} >
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuhtProvider>
   )
 }
 
