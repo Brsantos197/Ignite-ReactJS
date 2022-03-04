f9c39fc99673414af6a1b58448751cc4750af936
Brsantos197 <brsantos197@gmail.com>
2022-03-04 02:28:42 -0300

    API Routes no Next.js | Salvando usuário no banco

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,23 @@ export default NextAuth({
       authorization: "https://github.com/login/oauth/authorize?scope=read:user+user:email"
     }),
   ],
+  secret: process.env.SIGNING_KEY,
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+      } catch (error) {
+        return false
+      }
+    },
+  }
 })
