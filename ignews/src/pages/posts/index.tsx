b2300a85342cb20cb9d23edface544c2314d9ce9
Brsantos197 <brsantos197@gmail.com>
2022-06-19 23:49:03 -0300

    Trabalhando com CMS | Página: Posts

---
+++ ignews/src/pages/posts/index.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>Posts | Ignews</Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a>
+            <time>19 de junho de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+          <a>
+            <time>19 de junho de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+          <a>
+            <time>19 de junho de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+          <a>
+            <time>19 de junho de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
