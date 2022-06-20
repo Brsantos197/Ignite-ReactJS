c85295ecf242ed27e75eb446d46ce3dbdec8078b
Brsantos197 <brsantos197@gmail.com>
2022-06-20 02:06:36 -0300

    Trabalhando com CMS | Navegação no menu att

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import logo from "../../../public/images/logo.svg"
+import Image from "next/image";
 
 export function Header() {
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image src={logo}alt="ig.news" />
         <nav>
           <Link href="/">
           <a className={styles.active}>Home</a>
